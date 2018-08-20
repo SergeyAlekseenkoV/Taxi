30d1d301dcdee709c6ba79dad56dae5faa352c5b
SergeyAlekseenkoV <alexsv_@ukr.net>
2018-08-20 13:19:05 +0300

    first commit

---
+++ scripts/jquery-scroll.js
@@ -0,0 +1,7 @@
+(function($){
+    $(window).load(function(){
+        $("body").mCustomScrollbar({
+              theme:"dark-thin"
+        });
+    });
+})(jQuery);
---
+++ scripts/script-menu.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById("myNavResponsive").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("myNavResponsive").style.width = "0%";
+}
